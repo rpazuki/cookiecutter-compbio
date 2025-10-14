a9ef0773c557a831753c36e6d42351312bea1430
Roozbeh H. Pazuki <rpazuki@gmail.com>
2025-10-14 13:28:08 +0100

    Experiments temp

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 import os
+import shutil
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
 
 def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
+
+def move_folder(source_filepath, destination_filepath):
+    
+    shutil.copytree(os.path.join(PROJECT_DIRECTORY, source_filepath), 
+                    os.path.join(PROJECT_DIRECTORY, destination_filepath))
 
 
 if __name__ == '__main__':
@@ -15,4 +21,6 @@ if __name__ == '__main__':
     
     if '{{ cookiecutter.manuscript_format }}' != 'LaTeX':
          remove_file('doc/references.bib')
+
+    move_folder('experiments_template', 'experiments')
 
