f06fc4fdaad251e75949ef9ae672c94aae43c8ae
Roozbeh H. Pazuki <rpazuki@gmail.com>
2025-10-14 14:42:28 +0100

    experiments temp

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -10,6 +10,10 @@ def remove_file(filepath):
 
 def create_folder(folder):
     os.mkdir(os.path.join(PROJECT_DIRECTORY, folder))
+
+def rename_brackets_in_folder_name(folder, folder_name):    
+    os.rename(os.path.join(PROJECT_DIRECTORY, folder, folder_name),
+              os.path.join(PROJECT_DIRECTORY, folder, folder_name.replace("[", "{").replace("]", "}")))
         
 def replace_brackets_in_file(filepath):
     with open(filepath, 'r') as f:
@@ -24,7 +28,10 @@ if __name__ == '__main__':
     
     remove_file('experiments/.keep')
     replace_brackets_in_file('experiments/experiments_template/cookiecutter.json')
-    create_folder('experiments/experiments_template/{{ cookiecutter.experiment_slug')
+    # We need to change the folder, to prevent cookiecutter bto change it in first place
+    create_folder('experiments/experiments_template/[[ cookiecutter.experiment_slug ]]')
+    rename_brackets_in_folder_name('experiments/experiments_template', '[[ cookiecutter.experiment_slug ]]')
+
     remove_file('data/.keep')
     
     if '{{ cookiecutter.manuscript_format }}' != 'LaTeX':
