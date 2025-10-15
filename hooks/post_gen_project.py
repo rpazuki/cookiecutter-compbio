4e7cdf71c4fe64cca4a7fed5063af020c1b67a80
Roozbeh H. Pazuki <rpazuki@gmail.com>
2025-10-15 14:57:15 +0100

    add share folder support for windows

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python
 import os
-import shutil
+import platform
+import subprocess
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
+def __run_command__(command:list) -> bool:
+    result = subprocess.run(command,
+                            shell=True,
+                            capture_output=True,
+                            text=True)        
+    # Print the output
+    if result.stderr == "":
+        print(result.stdout)        
+        return True
+    else:
+        print(result.stderr)
+        return False
 
 def remove_file(filepath:str):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath)) 
@@ -25,8 +38,22 @@ def remove_dollar_sign_in_file(filepath:str):
         f.write(new_content)
 
 
-if __name__ == '__main__':    
-
+if __name__ == '__main__':   
+    #
+    remove_file('experiments/.keep')            
+    if '{{ cookiecutter.manuscript_format }}' != 'LaTeX':
+        remove_file('doc/references.bib')
+    # 
+    if '{{ cookiecutter.shared_data_folder.lower().strip() }}' != '':          
+        if platform.system()  == "Windows":             
+            if __run_command__(["cmd", "/c", "mklink", "/D", "data", '{{ cookiecutter.shared_data_folder }}']) == False:
+                print("Failed to create a symbolic link. Please run the command below in the command prompt (cmd) as an administrator:")
+                print(f'mklink /D "{os.path.join(PROJECT_DIRECTORY,"data")}" "{os.path.abspath("{{ cookiecutter.shared_data_folder }}")}"')                                 
+        else: #TODO: write for other os
+            os.symlink(os.path.abspath('{{ cookiecutter.shared_data_folder }}'), 
+                       os.path.join(PROJECT_DIRECTORY,'data'), target_is_directory=True)
+    else:
+        create_folder('data')
     #
     create_folder('experiments/template/{${cookiecutter.experiment_slug}$}')
     create_folder('experiments/template/{${cookiecutter.experiment_slug}$}/reports')
@@ -40,12 +67,7 @@ if __name__ == '__main__':
     remove_dollar_sign_in_file('experiments/template/cookiecutter.json')    
     rename_brackets_in_folder_name('experiments/template', '{${cookiecutter.experiment_slug}$}')
     remove_dollar_sign_in_file('experiments/template/hooks/post_gen_project.py')
-    #    
-    #
-    remove_file('experiments/.keep')    
-    remove_file('data/.keep')    
-    if '{{ cookiecutter.manuscript_format }}' != 'LaTeX':
-        remove_file('doc/references.bib')
+    #        
 
     
 
