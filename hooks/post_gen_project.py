8d6a94b7b93e864883aafb3a0be26e669820516c
Roozbeh Pazuki <rpazuki@gmail.com>
2025-10-16 16:57:12 +0100

    Linting the py codes

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,73 +6,96 @@ import subprocess
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
-def __run_command__(command:list) -> bool:
+
+def __run_command__(command: list) -> bool:
     result = subprocess.run(command,
                             shell=True,
                             capture_output=True,
-                            text=True)        
+                            text=True,
+                            check=False)
     # Print the output
     if result.stderr == "":
-        print(result.stdout)        
+        print(result.stdout)
         return True
     else:
         print(result.stderr)
         return False
-    
 
-def remove_file(filepath:str):
-    os.remove(os.path.join(PROJECT_DIRECTORY, filepath)) 
 
-def create_folder(folder:str):
+def remove_file(filepath: str):
+    """Remove a file given its relative path from the project directory."""
+    os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
+
+
+def create_folder(folder: str):
+    """Create a folder given its relative path from the project directory."""
     os.mkdir(os.path.join(PROJECT_DIRECTORY, folder))
 
-def rename_brackets_in_folder_name(folder:str, folder_name:str):    
+
+def rename_brackets_in_folder_name(folder: str, folder_name: str):
+    """Rename a folder by removing '$' from its name."""
     os.rename(os.path.join(PROJECT_DIRECTORY, folder, folder_name),
-              os.path.join(PROJECT_DIRECTORY, folder, folder_name.replace('$', ''))
+              os.path.join(PROJECT_DIRECTORY, folder,
+                           folder_name.replace('$', ''))
               )
-        
-def remove_dollar_sign_in_file(filepath:str):
-    with open(filepath, 'r') as f:
+
+
+def remove_dollar_sign_in_file(filepath: str):
+    """Remove 'dollar_sign' from a file given its relative path."""
+    with open(filepath, 'r', encoding='utf-8') as f:
         content = f.read()
 
     new_content = content.replace('$', '')
-    with open(filepath, 'w') as f:
+    with open(filepath, 'w', encoding='utf-8') as f:
         f.write(new_content)
 
 
-if __name__ == '__main__':   
+if __name__ == '__main__':
     #
-    remove_file('experiments/.keep')            
+    remove_file('experiments/.keep')
     if '{{ cookiecutter.manuscript_format }}' != 'LaTeX':
         remove_file('doc/references.bib')
     # mapp the data folder to a shared folder if specified
-    if '{{ cookiecutter.shared_data_folder.lower().strip() }}' != '':          
-        if platform.system()  == "Windows":             
-            if __run_command__(["cmd", "/c", "mklink", "/D", "data", '{{ cookiecutter.shared_data_folder }}']) == False:
-                print("Failed to create a symbolic link. Please run the command below in the command prompt (cmd) as an administrator:")
-                print(f'mklink /D "{os.path.join(PROJECT_DIRECTORY,"data")}" "{os.path.abspath("{{ cookiecutter.shared_data_folder }}")}"')                                 
-        else: #TODO: write for other os
-            os.symlink(os.path.abspath('{{ cookiecutter.shared_data_folder }}'), 
-                       os.path.join(PROJECT_DIRECTORY,'data'), target_is_directory=True)
+    if '{{ cookiecutter.shared_data_folder.lower().strip() }}' != '':
+        if platform.system() == "Windows":
+            if not __run_command__(["cmd",
+                                    "/c",
+                                    "mklink",
+                                    "/D",
+                                    "data",
+                                    '{{ cookiecutter.shared_data_folder }}']):
+                print("Failed to create a symbolic link. "
+                      "Please run the command below in the command prompt "
+                      "(cmd) as an administrator:")
+                print(
+                    f'mklink /D "{os.path.join(PROJECT_DIRECTORY,"data")}" '
+                    f'"{os.path.abspath("{{ cookiecutter.shared_data_folder }}")}"')
+        else:  # TODO: write for other os
+            os.symlink(os.path.abspath('{{ cookiecutter.shared_data_folder }}'),
+                       os.path.join(PROJECT_DIRECTORY, 'data'),
+                       target_is_directory=True)
     else:
         create_folder('data')
     #
     create_folder('experiments/template/{${cookiecutter.experiment_slug}$}')
-    create_folder('experiments/template/{${cookiecutter.experiment_slug}$}/reports')
-    create_folder('experiments/template/{${cookiecutter.experiment_slug}$}/reports/figures')
-    create_folder('experiments/template/{${cookiecutter.experiment_slug}$}/models')
-    create_folder('experiments/template/{${cookiecutter.experiment_slug}$}/notebooks')
-    create_folder('experiments/template/{${cookiecutter.experiment_slug}$}/processed')
-    
-    # If the name of a folder contains '{}', the cookiecutter will try to change it. Since we need the
-    # 'template' folder for calling the cookiecutter later, we uses '$' around the
-    # '{}', and remove them eventually.
-    remove_dollar_sign_in_file('experiments/template/cookiecutter.json')    
-    rename_brackets_in_folder_name('experiments/template', '{${cookiecutter.experiment_slug}$}')
-    remove_dollar_sign_in_file('experiments/template/hooks/post_gen_project.py')
-    #        
+    create_folder(
+        'experiments/template/{${cookiecutter.experiment_slug}$}/reports')
+    create_folder(
+        'experiments/template/{${cookiecutter.experiment_slug}$}/reports/figures')
+    create_folder(
+        'experiments/template/{${cookiecutter.experiment_slug}$}/models')
+    create_folder(
+        'experiments/template/{${cookiecutter.experiment_slug}$}/notebooks')
+    create_folder(
+        'experiments/template/{${cookiecutter.experiment_slug}$}/processed')
 
-    
-
-   
-
+    # If the name of a folder contains '{}', the cookiecutter will try
+    # to change it. Since we need the
+    # 'template' folder for calling the cookiecutter later,
+    # we uses '$' around the '{}', and remove them eventually.
+    remove_dollar_sign_in_file('experiments/template/cookiecutter.json')
+    rename_brackets_in_folder_name(
+        'experiments/template', '{${cookiecutter.experiment_slug}$}')
+    remove_dollar_sign_in_file(
+        'experiments/template/hooks/post_gen_project.py')
+    #
--- {{cookiecutter.project_slug}}/experiments/template/hooks/post_gen_project.py
+++ {{cookiecutter.project_slug}}/experiments/template/hooks/post_gen_project.py
@@ -5,17 +5,21 @@ import subprocess
 import shutil
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
-EXPERIMENTS_DIRECTORY = os.path.abspath(os.path.join(os.path.curdir, os.pardir))
-MAIN_DIRECTORY = os.path.abspath(os.path.join(os.path.curdir, os.pardir, os.pardir))
+EXPERIMENTS_DIRECTORY = os.path.abspath(
+    os.path.join(os.path.curdir, os.pardir))
+MAIN_DIRECTORY = os.path.abspath(
+    os.path.join(os.path.curdir, os.pardir, os.pardir))
 
-def __run_command__(command:list) -> bool:
+
+def __run_command__(command: list) -> bool:
     result = subprocess.run(command,
                             shell=True,
                             capture_output=True,
-                            text=True)        
+                            text=True,
+                            check=False)
     # Print the output
     if result.stderr == "":
-        print(result.stdout)        
+        print(result.stdout)
         return True
     else:
         print(result.stderr)
@@ -24,15 +28,18 @@ def __run_command__(command:list) -> bool:
 
 def __get_last_experiment_dir__(experiments_dir, except_dir=None):
     # List all directories in experiments_dir
-    dirs = [d for d in os.listdir(experiments_dir) 
-            if os.path.isdir(os.path.join(experiments_dir, d)) and d != os.path.basename(except_dir)]
+    dirs = [d for d in os.listdir(experiments_dir)
+            if os.path.isdir(os.path.join(experiments_dir, d)) and
+            d != os.path.basename(except_dir)]
     # Sort by modification time (most recent last)
     dirs.sort(key=lambda d: os.path.getmtime(os.path.join(experiments_dir, d)))
     # Return the most recent directory name, or None if none found
     return dirs[-1] if dirs else None
 
+
 def __copy_directory_contents__(src_dir, dst_dir):
-    """Copy all files and folders from src_dir to dst_dir, excluding names starting with a dot."""
+    """Copy all files and folders from src_dir to dst_dir, excluding names
+    starting with a dot."""
     if not os.path.exists(dst_dir):
         os.makedirs(dst_dir)
     for item in os.listdir(src_dir):
@@ -45,116 +52,153 @@ def __copy_directory_contents__(src_dir, dst_dir):
         else:
             shutil.copy2(s, d)
 
+
 def __copy_file__(src_file, dst_file):
-    """Move a file from src_file to dst_file."""    
-    shutil.copy2(src_file, dst_file)    
+    """Move a file from src_file to dst_file."""
+    shutil.copy2(src_file, dst_file)
 
 
-def __replace_in_file__(filepath:str, pattern:str, replacement:str):
-    with open(filepath, 'r') as f:
+def __replace_in_file__(filepath: str, pattern: str, replacement: str):
+    with open(filepath, 'r', encoding='utf-8') as f:
         content = f.read()
 
     new_content = content.replace(pattern, replacement)
-    with open(filepath, 'w') as f:
+    with open(filepath, 'w', encoding='utf-8') as f:
         f.write(new_content)
 
+
 def copy_default_files():
-    src_file = os.path.join(EXPERIMENTS_DIRECTORY, "template", "notebook_example.ipynb")
-    dst_file =  os.path.join(PROJECT_DIRECTORY, "notebooks", "notebook_example.ipynb")
+    """Copy default files to the new project"""
+    src_file = os.path.join(EXPERIMENTS_DIRECTORY,
+                            "template", "notebook_example.ipynb")
+    dst_file = os.path.join(
+        PROJECT_DIRECTORY, "notebooks", "notebook_example.ipynb")
     __copy_file__(src_file,
-                  dst_file)        
-    #    
-    
-    return True         
+                  dst_file)
+    #
+
+    return True
+
 
 def install_python_env() -> bool:
     """Create a local python environment"""
     if '{${ cookiecutter.create_python_env.lower().strip() }$}' == 'y':
         # Create a python environment
-        print("Begin creating a python environment in '{${ cookiecutter.python_env_name }$}' ...")
-        # IMPROTANT: do not remove the list. Since brackets turn into curly bracket, we need it here.
-        if __run_command__(["python", "-m",  "venv", "{${ cookiecutter.python_env_name }$}"]):        
+        print(
+            "Begin creating a python environment in "
+            "'{${ cookiecutter.python_env_name }$}' ...")
+        # IMPROTANT: do not remove the list.
+        # Since brackets turn into curly bracket, we need it here.
+        if __run_command__(["python", "-m",  "venv",
+                            "{${ cookiecutter.python_env_name }$}"]):
             print("python environment is created.")
-            return True               
+            return True
         else:
             return False
-        
+
     return True
 
 
 def install_requirements() -> bool:
     """Install packages that are listed in """
     if '{${ cookiecutter.install_packages.lower().strip() }$}' == 'y':
-       # Activate the python requirements
-       if platform.system()  == "Windows":
-           python_path = os.path.join(PROJECT_DIRECTORY,"{${ cookiecutter.python_env_name }$}", "Scripts", "python.exe")
-       else:
-           python_path = os.path.join(PROJECT_DIRECTORY,"{${ cookiecutter.python_env_name }$}", "bin", "python")
-       # Updating pip
-       print("Updating 'pip' ...")       
-       if __run_command__([python_path, "-m", "pip", "install",  "-U", "pip"]) == False:
-           return False
-       # Installing python packages from 'requirements.txt'                
-       req_path = os.path.join(EXPERIMENTS_DIRECTORY,"requirements.txt")
-       print(f"Installing python packages from '{req_path}' ...")
-       if __run_command__([python_path, "-m", "pip", "install",  "-r", req_path]):        
-           print("python packages are installed.")
-           return True
-       else:
-           return False
-        
+        # Activate the python requirements
+        if platform.system() == "Windows":
+            python_path = os.path.join(
+                PROJECT_DIRECTORY,
+                "{${ cookiecutter.python_env_name }$}",
+                "Scripts",
+                "python.exe")
+        else:
+            python_path = os.path.join(
+                PROJECT_DIRECTORY,
+                "{${ cookiecutter.python_env_name }$}",
+                "bin",
+                "python")
+        # Updating pip
+        print("Updating 'pip' ...")
+        if not __run_command__([python_path, "-m",
+                                "pip", "install", "-U", "pip"]):
+            return False
+        # Installing python packages from 'requirements.txt'
+        req_path = os.path.join(EXPERIMENTS_DIRECTORY, "requirements.txt")
+        print(f"Installing python packages from '{req_path}' ...")
+        if __run_command__([python_path,
+                            "-m",
+                            "pip",
+                            "install",
+                            "-r",
+                            req_path]):
+            print("python packages are installed.")
+            return True
+        else:
+            return False
+
     return True
 
+
 def install_local_lib() -> bool:
+    """Install the local library in editable mode in src folder"""
     # Activate the python environment
-    if platform.system()  == "Windows":
-        python_path = os.path.join(PROJECT_DIRECTORY,"{${ cookiecutter.python_env_name }$}", "Scripts", "python.exe")
+    if platform.system() == "Windows":
+        python_path = os.path.join(
+            PROJECT_DIRECTORY,
+            "{${ cookiecutter.python_env_name }$}",
+            "Scripts",
+            "python.exe")
     else:
-        python_path = os.path.join(PROJECT_DIRECTORY,"{${ cookiecutter.python_env_name }$}", "bin", "python")            
-    if platform.system()  == "Windows":        
-        src_path = MAIN_DIRECTORY + "\\src\\" +  "{{ cookiecutter.module_name }}"
+        python_path = os.path.join(
+            PROJECT_DIRECTORY,
+            "{${ cookiecutter.python_env_name }$}",
+            "bin",
+            "python")
+    if platform.system() == "Windows":
+        src_path = MAIN_DIRECTORY + "\\src\\" + \
+            "{{ cookiecutter.module_name }}"
     else:
-        src_path =   MAIN_DIRECTORY + "/src/" +  "{{ cookiecutter.module_name }}"
-    
+        src_path = MAIN_DIRECTORY + "/src/" + "{{ cookiecutter.module_name }}"
+
     print(f"Install local package '{src_path}' ...")
-    return __run_command__([python_path, "-m",  "pip", "install", "-e", src_path])  
+    return __run_command__([python_path,
+                            "-m",
+                            "pip",
+                            "install",
+                            "-e",
+                            src_path])
+
 
 def copy_last_experiment() -> bool:
     """copy all files from the last experiment to the new one"""
     if '{${ cookiecutter.copy_last_experiment.lower().strip() }$}' == 'n':
         return True
-    
+
     # copy the last experiment files
-    last_experiment = __get_last_experiment_dir__(EXPERIMENTS_DIRECTORY, PROJECT_DIRECTORY)
+    last_experiment = __get_last_experiment_dir__(
+        EXPERIMENTS_DIRECTORY, PROJECT_DIRECTORY)
+    if not last_experiment:
+        print("No previous experiment directory found to copy from.")
+        return True
+
     print(f"Copy the last experiment ('{last_experiment}') files ...")
     try:
-        __copy_directory_contents__(os.path.join(EXPERIMENTS_DIRECTORY,last_experiment), PROJECT_DIRECTORY)
+        __copy_directory_contents__(os.path.join(
+            EXPERIMENTS_DIRECTORY, last_experiment), PROJECT_DIRECTORY)
         return True
-    except Exception as e:
-        print(f"Error copying files from '{last_experiment}' to '{PROJECT_DIRECTORY}': {e}")
+    except (OSError, shutil.Error) as e:
+        print(
+            f"Error copying files from '{last_experiment}' "
+            f"to '{PROJECT_DIRECTORY}': {e}")
         return False
-    
-    
-    
-    
+
+
 if __name__ == '__main__':
     commands = [copy_default_files,
                 install_python_env,
                 install_requirements,
                 install_local_lib,
                 copy_last_experiment]
-    ret = True
+    RET = True
     # Execute commands if the previous one was successful
-    while ret == True and len(commands) > 0:
+    while RET is True and len(commands) > 0:
         c = commands.pop(0)
-        ret = c()
-        
-     
-            
-        
-   
-
-    
-
-   
-
+        RET = c()
