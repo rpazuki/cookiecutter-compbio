ed1d6f49e1bf8f93c73f2f5a530757c617bd3869
Roozbeh H. Pazuki <rpazuki@gmail.com>
2025-10-27 13:16:27 +0000

    More resilient search for directories

--- {{cookiecutter.project_slug}}/src/{{ cookiecutter.module_name }}/directories.py
+++ {{cookiecutter.project_slug}}/src/{{ cookiecutter.module_name }}/directories.py
@@ -1,8 +1,16 @@
 # pylint: skip-file
 import os
+from pathlib import Path
 
 SHARED_DATA_DIRECTORY = "{{ cookiecutter.shared_data_directory}}"
-EXPERIMENTS_DIRECTORY = os.path.realpath(os.path.curdir)
+EXPERIMENTS_DIRECTORY = Path(os.path.realpath(os.path.curdir))
+while (EXPERIMENTS_DIRECTORY.name.lower() != "experiments" and
+       not EXPERIMENTS_DIRECTORY.parent is None and
+       EXPERIMENTS_DIRECTORY != EXPERIMENTS_DIRECTORY.parent):
+    EXPERIMENTS_DIRECTORY = EXPERIMENTS_DIRECTORY.parent
+
+if EXPERIMENTS_DIRECTORY == EXPERIMENTS_DIRECTORY.parent: # root directory reached
+    raise ValueError("There is no experiments folder.")
 
 if SHARED_DATA_DIRECTORY == '':
     DATA_DIRECTORY = os.path.join(EXPERIMENTS_DIRECTORY, os.pardir, os.pardir, 'data')
